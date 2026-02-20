16c084019590df4a6d84fb51e4c0a86367c9aed9
Valentin <valentin.longagnani@outlook.com>
2026-02-20 10:26:29 +0100

    fix: quality of life

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,79 +19,91 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
     }
 end)
 
-config = {
-    automatically_reload_config = true,
-    enable_tab_bar = true,
-    hide_tab_bar_if_only_one_tab = true,
-    use_fancy_tab_bar = false,
-    window_close_confirmation = "NeverPrompt",
-    window_decorations = "RESIZE",
-    default_cursor_style = "BlinkingBar",
-    color_scheme = "Nord (Gigh)",
-    font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
-    font_size = 12.5,
-    background = {
-        {
-            source = {
-                File = "C:\\Users\\Valentin\\bg-monterey.png"
-            },
-            hsb = {
-                hue = 1.0,
-                saturation = 1.02,
-                brightness = 0.25,
-            },
-            width = "100%",
-            height = "100%"
-        },
-        {
-            source = { Color = "#282c35" },
-            width = "100%",
-            height = "100%",
-            opacity = 0.75,
-        }
-    },
-    -- Pane actif visible
-    inactive_pane_hsb = {
-        saturation = 0.4,
-        brightness = 0.4,
-    },
-    colors = {
-        -- split = "#7aa2f7",
-        tab_bar = {
-            background = "#1a1b26",
-        },
-    },
-    window_padding = {
-        left = 3,
-        right = 3,
-        top = 0,
-        bottom = 0,
-    },
-    keys = {
-        -- Onglets
-        { key = "t", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab "CurrentPaneDomain" },
-        { key = "w", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab { confirm = false } },
-        { key = "Tab", mods = "CTRL", action = wezterm.action.ActivateTabRelative(1) },
-        { key = "Tab", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
-        { key = "1", mods = "CTRL", action = wezterm.action.ActivateTab(0) },
-        { key = "2", mods = "CTRL", action = wezterm.action.ActivateTab(1) },
-        { key = "3", mods = "CTRL", action = wezterm.action.ActivateTab(2) },
+config.default_prog = { "pwsh", "-NoLogo" }
+config.automatically_reload_config = true
 
-        -- Splits
-        { key = "h", mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
-        { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
-        { key = "LeftArrow",  mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection "Left" },
-        { key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection "Right" },
-        { key = "UpArrow",    mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection "Up" },
-        { key = "DownArrow",  mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection "Down" },
-        { key = "x", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentPane { confirm = false } },
+-- Onglets
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 
-        -- Redimensionner les splits
-{ key = "LeftArrow",  mods = "CTRL|ALT", action = wezterm.action.AdjustPaneSize { "Left", 5 } },
-{ key = "RightArrow", mods = "CTRL|ALT", action = wezterm.action.AdjustPaneSize { "Right", 5 } },
-{ key = "UpArrow",    mods = "CTRL|ALT", action = wezterm.action.AdjustPaneSize { "Up", 5 } },
-{ key = "DownArrow",  mods = "CTRL|ALT", action = wezterm.action.AdjustPaneSize { "Down", 5 } },
+-- Fenêtre
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
+config.window_padding = {
+    left = 3,
+    right = 3,
+    top = 0,
+    bottom = 0,
+}
+
+-- Curseur
+config.default_cursor_style = "BlinkingBar"
+
+-- Apparence
+config.color_scheme = "Nord (Gogh)"
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
+config.font_size = 12.5
+
+-- Fond d'écran
+config.background = {
+    {
+        source = { File = "C:\\Outils\\wezterm\\bg-monterey-small.png" },
+        hsb = {
+            hue = 1.0,
+            saturation = 1.02,
+            brightness = 0.25,
+        },
+        width = "100%",
+        height = "100%",
     },
+    {
+        source = { Color = "#282c35" },
+        width = "100%",
+        height = "100%",
+        opacity = 0.75,
+    },
+}
+
+-- Panes
+config.inactive_pane_hsb = {
+    saturation = 0.4,
+    brightness = 0.4,
+}
+
+-- Couleurs
+config.colors = {
+    tab_bar = {
+        background = "#1a1b26",
+    },
+}
+
+-- Raccourcis clavier
+config.keys = {
+    -- Onglets
+    { key = "t", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab "CurrentPaneDomain" },
+    { key = "w", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab { confirm = false } },
+    { key = "Tab", mods = "CTRL",       action = wezterm.action.ActivateTabRelative(1) },
+    { key = "Tab", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
+    { key = "1",   mods = "CTRL",       action = wezterm.action.ActivateTab(0) },
+    { key = "2",   mods = "CTRL",       action = wezterm.action.ActivateTab(1) },
+    { key = "3",   mods = "CTRL",       action = wezterm.action.ActivateTab(2) },
+    { key = "m", mods = "CTRL|SHIFT", action = wezterm.action.Hide },
+
+    -- Splits
+    { key = "h", mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+    { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical   { domain = "CurrentPaneDomain" } },
+    { key = "LeftArrow",  mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection "Left" },
+    { key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection "Right" },
+    { key = "UpArrow",    mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection "Up" },
+    { key = "DownArrow",  mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection "Down" },
+    { key = "x", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentPane { confirm = false } },
+
+    -- Redimensionner les splits
+    { key = "LeftArrow",  mods = "CTRL|ALT", action = wezterm.action.AdjustPaneSize { "Left",  5 } },
+    { key = "RightArrow", mods = "CTRL|ALT", action = wezterm.action.AdjustPaneSize { "Right", 5 } },
+    { key = "UpArrow",    mods = "CTRL|ALT", action = wezterm.action.AdjustPaneSize { "Up",    5 } },
+    { key = "DownArrow",  mods = "CTRL|ALT", action = wezterm.action.AdjustPaneSize { "Down",  5 } },
 }
 
 return config
